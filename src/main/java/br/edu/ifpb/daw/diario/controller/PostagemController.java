9cc6bfff82b8e4450729413afb3d71655f80b9ee
CamaraEduarda <eduarda.camara@academico.ifpb.edu.br>
2025-04-17 20:09:27 -0300

    feat[service] Criação da classe PostagemController e de seus endpoints

---
+++ src/main/java/br/edu/ifpb/daw/diario/controller/PostagemController.java
@@ -0,0 +1,61 @@
+package br.edu.ifpb.daw.diario.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import br.edu.ifpb.daw.diario.dto.PostagemDTO;
+import br.edu.ifpb.daw.diario.dto.PostagemResponse;
+import br.edu.ifpb.daw.diario.service.PostagemService;
+
+@RestController
+@RequestMapping("/postagens")
+public class PostagemController {
+
+    private PostagemService postagemService;
+
+    @GetMapping("/api/postagens")
+    public ResponseEntity<List<PostagemResponse>> listarPostagens() {
+        List<PostagemResponse> postagens = postagemService.listarTodas();
+        return ResponseEntity.ok(postagens);
+    }
+
+    @GetMapping("/api/postagens/{id}")
+    public ResponseEntity<PostagemResponse> buscarPostagemPorId(@PathVariable Long id){
+        Optional<PostagemResponse> postagem = postagemService.buscarPostagemPorId(id);
+        return postagem.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());  
+    }
+
+    @PostMapping
+    public ResponseEntity<PostagemResponse> salvarPostagem(@RequestParam("titulo") String titulo, @RequestParam("texto") String texto, @RequestParam("imagem") String fotoUrl) {
+        PostagemDTO postagemDTO = new PostagemDTO(titulo, texto, fotoUrl);
+        PostagemResponse postagemSalva = postagemService.salvar(postagemDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(postagemSalva);
+    }
+
+    @PutMapping("/api/postagens/{id}")
+     public ResponseEntity<PostagemResponse> atualizarPostagem(@PathVariable Long id, @RequestParam("titulo") String titulo, @RequestParam("texto") String texto, @RequestParam("imagem") String fotoUrl) {
+        PostagemDTO postagemDTO = new PostagemDTO(titulo, texto, fotoUrl);
+        Optional<PostagemResponse> postagemAtualizada = postagemService.atualizar(id, postagemDTO);
+        return postagemAtualizada.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+
+    @DeleteMapping("/api/postagens/{id}")
+    public ResponseEntity<Void> deletarPostagem(@PathVariable Long id) {
+        postagemService.excluir(id);
+        return ResponseEntity.noContent().build();
+    }
+    
+}

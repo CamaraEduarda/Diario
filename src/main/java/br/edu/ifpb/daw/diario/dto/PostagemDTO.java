938a362d9a1d10b360e725f5025b140c2f2f415f
meduarda18 <mariaeduardac604@gmail.com>
2025-04-21 14:44:12 -0300

    Validações necessárias  adicionadas

--- src/main/java/br/edu/ifpb/daw/diario/dto/PostagemDTO.java
+++ src/main/java/br/edu/ifpb/daw/diario/dto/PostagemDTO.java
@@ -1,10 +1,19 @@
 package br.edu.ifpb.daw.diario.dto;
 
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class PostagemDTO {
-    
+
+    @NotBlank(message = "O título não pode estar em branco.")
+    @Size(max = 50, message = "O título deve ter no máximo 50 caracteres")
     private String titulo;
+
+    @NotBlank(message = "O texto não pode estar em branco.")
     private String texto;
+
+    @NotBlank(message = "A URL da imagem não pode estar em branco.")
     private String imagemUrl;
 
     public PostagemDTO(String titulo, String texto, String imagemUrl) {

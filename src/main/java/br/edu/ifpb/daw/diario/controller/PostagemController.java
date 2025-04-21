d8a109f3fadebcc6358e12cb615eab052b289ea5
meduarda18 <mariaeduardac604@gmail.com>
2025-04-21 14:00:15 -0300

    Ajustes nas requisições

--- src/main/java/br/edu/ifpb/daw/diario/controller/PostagemController.java
+++ src/main/java/br/edu/ifpb/daw/diario/controller/PostagemController.java
@@ -6,14 +6,7 @@ import java.util.Optional;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import br.edu.ifpb.daw.diario.dto.PostagemDTO;
@@ -43,15 +36,14 @@ public class PostagemController {
     }
 
     @PostMapping
-    public ResponseEntity<PostagemResponse> salvarPostagem(@RequestParam("titulo") String titulo, @RequestParam("texto") String texto, @RequestParam("imagem") String fotoUrl) {
-        PostagemDTO postagemDTO = new PostagemDTO(titulo, texto, fotoUrl);
+    public ResponseEntity<PostagemResponse> salvarPostagem(@RequestBody PostagemDTO postagemDTO) {
+
         PostagemResponse postagemSalva = postagemService.salvar(postagemDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(postagemSalva);
     }
 
     @PutMapping("/{id}")
-     public ResponseEntity<PostagemResponse> atualizarPostagem(@PathVariable Long id, @RequestParam("titulo") String titulo, @RequestParam("texto") String texto, @RequestParam("imagem") String fotoUrl) {
-        PostagemDTO postagemDTO = new PostagemDTO(titulo, texto, fotoUrl);
+     public ResponseEntity<PostagemResponse> atualizarPostagem(@PathVariable Long id, @RequestBody PostagemDTO postagemDTO) {
         Optional<PostagemResponse> postagemAtualizada = postagemService.atualizar(id, postagemDTO);
         return postagemAtualizada.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }

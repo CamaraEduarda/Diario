dad42f24cf01bdbc758b9982cd0162c2378d19ee
meduarda18 <mariaeduardac604@gmail.com>
2025-04-23 13:56:15 -0300

    apaguei o método buscar por título do repository

--- src/main/java/br/edu/ifpb/daw/diario/repository/PostagemRepository.java
+++ src/main/java/br/edu/ifpb/daw/diario/repository/PostagemRepository.java
@@ -3,8 +3,6 @@ package br.edu.ifpb.daw.diario.repository;
 import br.edu.ifpb.daw.diario.entity.Postagem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
 
 }

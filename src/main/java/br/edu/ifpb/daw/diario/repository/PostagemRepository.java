00340cccb10c18524848206532f81020b37561fe
meduarda18 <mariaeduardac604@gmail.com>
2025-04-23 13:34:25 -0300

    apaguei o método buscar por título do repository

--- src/main/java/br/edu/ifpb/daw/diario/repository/PostagemRepository.java
+++ src/main/java/br/edu/ifpb/daw/diario/repository/PostagemRepository.java
@@ -6,6 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
-    //Esse metodo é uma consulta direta no banco de dados, por isso está no repository, o service ficará responsável por usá-la.
-    List<Postagem> findByTituloContainingIgnoreCase(String titulo);
+
 }
